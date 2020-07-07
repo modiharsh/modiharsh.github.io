2ce40f759a5c4f299477adab25d0d4e71a249f43
Harsh Modi <modiharsh21@gmail.com>
2020-07-07 22:59:12 +0530

    Added score, eyes to snake

--- Snake/snake.js
+++ Snake/snake.js
@@ -13,11 +13,14 @@ var rightDirection = false;
 var upDirection = false;
 var downDirection = false;
 var speed = 120;
+var score = 0;
 
 
 function init(){
     canv = document.getElementById("myCanvas");
+    currScore = document.getElementById("score");
     ctx = canv.getContext("2d");
+    displayButtons();
     document.addEventListener("keydown",keyPush);
     document.addEventListener("touchstart", touchStartHandler, false);
     document.addEventListener("touchend", touchEndHandler, false);
@@ -25,6 +28,38 @@ function init(){
     game();
 }
 
+
+function displayButtons(){
+    var isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function() {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+        }
+    };    
+
+    if( isMobile.any() ){ alert('Mobile');}
+    else{
+        mobileButtonDiv = document.getElementById("mobileButtons");
+        document.getElementById("mobileButtons").style.display = "none";
+    }
+
+
+
+}
 
 function game(){
     px+=xv;
@@ -48,10 +83,56 @@ function game(){
     for(var i=0;i<trail.length;i++){
         ctx.fillRect(trail[i].x *gs , trail[i].y * gs, gs-2,gs-2);
         if(trail[i].x ==px && trail[i].y==py){
-            tail =3 ;
+            tail = 3;
             speed = 120;
+            score = 0;
+            currScore.innerHTML = score;
         }
     }
+
+    ctx.fillStyle="white";
+    for(var i=0;i<trail.length;i++){
+        if( i == trail.length-1){
+            if( rightDirection == true){
+                ctx.fillRect(trail[i].x *gs + 12 , trail[i].y * gs + 4 , 3,3); // TR
+                ctx.fillRect(trail[i].x *gs + 12 , trail[i].y * gs + 12 , 3,3); //BR
+                //ctx.fillStyle="red";
+                //ctx.fillRect(trail[trail.length-1].x *gs + 18 , trail[trail.length-1].y * gs + 8 , 3,3); //T
+            }
+            else if(leftDirection == true){
+                ctx.fillRect(trail[i].x *gs + 4 , trail[i].y * gs + 4 , 3,3); //TL
+                ctx.fillRect(trail[i].x *gs + 4 , trail[i].y * gs + 12 , 3,3); //BL
+                //ctx.fillStyle="red";
+                //ctx.fillRect(trail[trail.length-1].x *gs  , trail[trail.length-1].y * gs + 8 , -3,3); //T
+            }
+            else if(upDirection == true){
+                ctx.fillRect(trail[i].x *gs + 4 , trail[i].y * gs + 4 , 3,3); //TL
+                ctx.fillRect(trail[i].x *gs + 12 , trail[i].y * gs + 4 , 3,3); // TR
+                //ctx.fillStyle="red";
+                //ctx.fillRect(trail[trail.length-1].x *gs + 8 , trail[trail.length-1].y * gs  , 3,-3); //T
+            }
+            else if(downDirection == true){
+                ctx.fillRect(trail[i].x *gs + 4 , trail[i].y * gs + 12 , 3,3); //BL
+                ctx.fillRect(trail[i].x *gs + 12 , trail[i].y * gs + 12 , 3,3); //BR
+                //ctx.fillStyle="red";
+                //ctx.fillRect(trail[trail.length-1].x *gs + 8 , trail[trail.length-1].y * gs + 18 , 3,3); //T
+            }
+            else{
+                continue;
+            }
+            
+
+
+//            ctx.fillRect(trail[i].x *gs + 12 , trail[i].y * gs + 12 , 3,3); //BR
+  //          ctx.fillRect(trail[i].x *gs + 4 , trail[i].y * gs + 12 , 3,3); //BL
+    //        ctx.fillRect(trail[i].x *gs + 12 , trail[i].y * gs + 4 , 3,3); // TR
+      //      ctx.fillRect(trail[i].x *gs + 4 , trail[i].y * gs + 4 , 3,3); //TL
+        }
+    }
+    
+
+    
+
     trail.push({x:px,y:py});
     while(trail.length > tail){
         trail.shift();
@@ -59,6 +140,8 @@ function game(){
     if(ax ==px && ay==py){
         tail++;
         speed-=2;
+        score+=10;
+        currScore.innerHTML = score;
         
         ax = Math.floor(Math.random()*tc);
         ay = Math.floor(Math.random()*tc);
@@ -76,6 +159,8 @@ function game(){
         alert("speed got zero");
         speed = 120;
     }
+    
+    currScore.innerHTML = score;
     setTimeout(game, speed);
 
 }
@@ -209,3 +294,5 @@ function touchEndHandler(event) {
     /* determine what gesture was performed, based on dx and dy (tap, swipe, one or two fingers etc. */
 
 }
+
+
